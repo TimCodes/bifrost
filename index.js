ef351a448617179a8ee8bfa0ac9e250e7137da8d
TimCodes <hardtimo@isu.edu>
2026-04-17 19:11:17 -0700

    chore: add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// bifrost - VulnFix POC test app
+
+const Handlebars = require('handlebars');
+const picomatch = require('picomatch');
+
+console.log('bifrost running - vulnerable deps: handlebars@4.7.8, picomatch@2.3.1');
